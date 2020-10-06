f77f0f495b894992fa56c4f496c3b60f126a789f
nhungtth <nhungtth@kaopiz.com>
2020-10-06 10:30:44 +0900

    add banner

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
-echo "Reminder is running!"
-
+echo "
+██████  ███████ ███    ███ ██ ███    ██ ██████  ███████     ███████ ████████  █████  ███    ██ ██████  ██    ██ ██████  
+██   ██ ██      ████  ████ ██ ████   ██ ██   ██ ██          ██         ██    ██   ██ ████   ██ ██   ██ ██    ██ ██   ██ 
+██████  █████   ██ ████ ██ ██ ██ ██  ██ ██   ██ █████ █████ ███████    ██    ███████ ██ ██  ██ ██   ██ ██    ██ ██████  
+██   ██ ██      ██  ██  ██ ██ ██  ██ ██ ██   ██ ██               ██    ██    ██   ██ ██  ██ ██ ██   ██ ██    ██ ██      
+██   ██ ███████ ██      ██ ██ ██   ████ ██████  ███████     ███████    ██    ██   ██ ██   ████ ██████   ██████  ██                                                                                                                             
+"
 while :
 do
 	currenttime=$(date +%H:%M)
 	echo $currenttime
    	if [[ "$currenttime" > "08:00" ]] && [[ "$currenttime" < "22:00" ]]; then
 		osascript -e 'display notification "Standup Time!" with title "Standup for your health and your eyes!"'
-
 		# Remind every each 5min
 		for i in 1 2 3 4 5 6 7 8 9
 	        do
@@ -17,7 +20,6 @@ do
 	            echo "$time minutes remaining!"
 	            sleep 300
 	        done
-
 	        echo "Standup! For your health and your eyes!"
 	else
 		echo "Its time for do not disturb mode. Try later!"
